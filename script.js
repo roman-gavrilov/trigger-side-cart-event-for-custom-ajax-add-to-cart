a91b62abf4aebb1fe3f49cc30784d4522545d092
Roman <roman.gavrilov.0309@gmail.com>
2021-10-13 18:07:00 -0400

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var duoProduct = document.querySelector('.duo-product');   
     
 const ajaxCart = () => {
-  const duo = {
+  const data = {
     id: duoProduct.dataset.variant,
     quantity: "1",
     form_type: "product",
@@ -11,7 +11,7 @@ const ajaxCart = () => {
   fetch("/cart/add.js", {
     method: 'POST',
     headers: new Headers({'content-type': 'application/json'}),
-    body: JSON.stringify(duo)
+    body: JSON.stringify(data)
   }).then(res => {
     document.querySelector('[data-ajax-cart-trigger]').classList.add('show-mini-cart');
     Shopify.theme.jsAjaxCart.updateView();
